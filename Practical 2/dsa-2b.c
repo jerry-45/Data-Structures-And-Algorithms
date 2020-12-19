4d01b0f31b9c2cd23860bc917d4c9469f00a8e69
jerry-45 <vishwa.atul16@gmail.com>
2020-12-19 23:02:54 +0530

    Practical 2

--- Practical 2/dsa-2b.c
+++ Practical 2/dsa-2b.c
@@ -1,59 +1,63 @@
-///2bofdatastructure//
+// Write a program to search the elements in the linked list and display the same
+
 #include<stdio.h>
 #include<conio.h>
 #include<stdlib.h> 
 #include<malloc.h>
+
 struct node
 {
-int data; 
-struct node*next;
-}
-first,*nw; 
+    int data; 
+    struct node*next;
+}first,*nw; 
+
 int search(int);
-void main()
+
+int main()
 {
-int no,i,item,pos;
-first.next=NULL; 
-nw=&first;
-printf("Enter The No of nodes,you want in linked list:"); 
-scanf("%d",&no); 
-printf("\n"); 
-for(i=0;i<no;i++)
-{
-nw->next=(struct node*)malloc(sizeof(struct node)); 
-printf("Enter element in node %d:",i+1); 
-scanf("%d",&nw->data); 
-nw=nw->next;
-}
-nw->next=NULL;
-printf("Linked list is:\n"); 
-nw=&first; 
-while(nw->next!=NULL)
-{
-printf("%d\t",nw->data); 
-nw=nw->next;
-}
-printf("\n"); 
-printf("Enter item to be searched:"); 
-scanf("%d",&item); 
-pos=search(item); 
-if(pos<=no) 
-printf("Your item is at node=%d",pos);
-else
-printf("Sorry!youritemisnotinlinkedlist.");
-getch();
-}
-int search(int item)
-{
-int count=1; 
-nw=&first; 
-while(nw->next!=NULL)
-{
-if(nw->data==item)
-break;
-else
-count++; nw=nw->next;
-}
-return count;
+    int no,i,item,pos;
+    first.next=NULL; 
+    nw=&first;
+    printf("Enter total number of nodes: "); 
+    scanf("%d",&no); 
+    printf("\n"); 
+    for(i=0;i<no;i++)
+    {
+        nw->next=(struct node*)malloc(sizeof(struct node)); 
+        printf("Enter element in node %d:",i+1); 
+        scanf("%d",&nw->data); 
+        nw=nw->next;
+    }
+    nw->next=NULL;
+    printf("Linked list is:\n"); 
+    nw=&first; 
+    while(nw->next!=NULL)
+    {
+        printf("%d\t",nw->data); 
+        nw=nw->next;
+    }
+    printf("\n"); 
+    printf("Enter item to be searched:"); 
+    scanf("%d",&item); 
+    pos=search(item); 
+    if(pos<=no) 
+        printf("Your item is at node=%d",pos);
+    else
+        printf("Sorry! your item is not in linked list.");
+    
+    return 0;
 }
 
+int search(int item)
+{
+    int count=1; 
+    nw=&first; 
+    while(nw->next!=NULL)
+    {
+        if(nw->data==item)
+            break;
+        else
+            count++; nw=nw->next;
+    }   
+    return count;
+}
--- README.md
+++ README.md
@@ -28,9 +28,9 @@ So far we have tried to keep all the code in the repo, big free. If you detected
 | 1             | A            | Write a program to store the elements in 1-D array and perform the operations like searching, sorting and reversing the elements. [Menu Driven]    | ☑ |
 | 1             | B            | Read the two arrays from the user and merge them and display the elements in sorted order.[Menu Driven]    | ☑ |
 | 1             | C            | Write a program to perform the Matrix addition, Multiplication and Transpose Operation. [Menu Driven]    | ☑ |
-| 2             | A            | Write a program to create a single linked list and display the node elements in reverse order.   |
-| 2             | B            | Write a program to search the elements in the linked list and display the same    |
-| 2             | C            | Write a program to create double linked list and sort the elements in the linked list.    |
+| 2             | A            | Write a program to create a single linked list and display the node elements in reverse order.   | ☑ |
+| 2             | B            | Write a program to search the elements in the linked list and display the same    | ☑ |
+| 2             | C            | Write a program to create double linked list and reverse the elements in the linked list.    | ☑ |
 | 3             | A            | Write a program to implement the concept of Stack with Push, Pop, Display and Exit operations.    |
 | 3             | B            | Write a program to convert an infix expression to postfix and prefix conversion.    |
 | 3             | C            | Write a program to implement Tower of Hanoi problem.    |
